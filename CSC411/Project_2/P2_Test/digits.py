cda4502c816314702309ed0f14c5d325510862e2
cheneyuwu <cheneyuwu@gmail.com>
2018-02-09 14:13:30 -0500

    411p2

--- CSC411/Project_2/P2_Test/digits.py
+++ CSC411/Project_2/P2_Test/digits.py
@@ -24,6 +24,9 @@ num_digits = 10
 # Functions used in multiple parts
 # -----------------------------------------------------------------------------
 def load_mnist():
+    """
+    load mnist data base and divide by 255
+    """
     M = loadmat("mnist_all.mat")
     for i in range(num_digits):
         M['train'+str(i)]=M['train'+str(i)]/255.
@@ -31,6 +34,9 @@ def load_mnist():
     return M
 
 def create_sets():
+    """
+    generate input and desired output arrays
+    """
     M = load_mnist()
     training_x = np.empty((0,input_size))
     test_x = np.empty((0,input_size))
@@ -48,34 +54,39 @@ def create_sets():
     return training_x, training_y, test_x, test_y
 
 def softmax_forward(y):
-    '''Return the output of the softmax function for the matrix of output y. y
-    is an NxM matrix where N is the number of outputs for a single case, and M
-    is the number of cases'''
-    return exp(y)/tile(sum(exp(y),0), (len(y),1))
-    
-def tanh_layer(y, W, b):    
-    '''Return the output of a tanh layer for the input matrix y. y
-    is an NxM matrix where N is the number of inputs for a single case, and M
-    is the number of cases'''
-    return tanh(dot(W.T, y)+b)
-
-def fully_connected_layer_forward(x,W,b):
-    """
-    Return fully connected layer 
+    '''
+    Return the output of the softmax function for the matrix of output y. 
     Input:
-        x: N*M where M = 784
-        W: 10*784
-        b: 10*1
-    Output:
-        fully connected N*10
-    """
-    return np.matmul(W,x.T)+b
+        y: NxM matrix where N is the number of outputs (10) for a single case, and M
+    is the number of cases
+    '''
+    return exp(y)/tile(sum(exp(y),0), (len(y),1))
 
 def softmax_backward(y,output):
     '''Return the output of the softmax function for the matrix of output y. y
     is an NxM matrix where N is the number of outputs for a single case, and M
     is the number of cases'''
     return output-y
+    
+def tanh_layer(y, W, b):    
+    '''
+    Return the output of a tanh layer for the input matrix y. y
+    is an NxM matrix where N is the number of inputs for a single case, and M
+    is the number of cases
+    '''
+    return tanh(dot(W.T, y)+b)
+
+def fully_connected_layer_forward(x,W,b):
+    """
+    Return output of fully connected layer 
+    Input:
+        x: N*M where M = 784
+        W: 10*784
+        b: 10*1
+    Output:
+        fully connected: 10*N    
+    """
+    return np.matmul(W,x.T)+b
 
 def fully_connected_layer_backward(dy,x):
     """
@@ -93,14 +104,51 @@ def fully_connected_layer_backward(dy,x):
     db = np.matmul(dy, np.ones([x.shape[0],1]))
     return dW,db
 
+def grad_descent(f, df, loss, x, y, init_t,ada_learning_rate = True,momentum = True, alpha=0.0001, max_iter=3000, EPS = 1e-5, damping = 0.9):
+    """
+    Input:
+        ada_learning_rate: adaptive learning rate flag
+        momentum: momentum flag
+        damping: momentum danping ratio
+    Output:
+        
+    """
+    v = 0
+    prev_t = init_t-10*EPS
+    t = init_t.copy()
+    currloss = loss(y, f(x,t))
+    curr_alpha = alpha
+    iter  = 0
+    while norm(t - prev_t) >  EPS and iter < max_iter:
+        prev_t = t.copy()
+        if momentum:
+            v = damping*v+curr_alpha*df(y, f(x,t), x)
+            t -= v
+        else:
+            t -= curr_alpha*df(y, f(x,t), x)
+        if iter % 500 == 0:
+            print "Iter", iter
+            if currloss < loss(y, f(x,t)) and ada_learning_rate:
+                curr_alpha = curr_alpha/2
+            currloss = loss(y, f(x,t))
+            print currloss
+            #print "Gradient: ", df(x, y, t), "\n"
+        iter += 1
+    return t
+
 def NLL(y, output):
+    """
+    Negative log loss function
+    """
     return -sum(y*log(output)) 
 
 # Part 1
 # -----------------------------------------------------------------------------
 def part1():
     """
-    
+    Done!
+    Print required figures
+    add axies and sub-titles if needed 
     """
     try:
         os.makedirs("figures")
@@ -125,29 +173,42 @@ def part1():
 # Part 2
 # -----------------------------------------------------------------------------    
 def part2_forward(x, W1, b1):
+    """
+    Forward pass of the nn built in part2
+    """
     L1 = fully_connected_layer_forward(x,W1,b1)
     output = softmax_forward(L1)
     return L1, output
 
 def part2():
-    
+    """
+    Done!
+    Nothing much to do for this function.
+    required funcitons are defined above.
+    """
     #create weights
     W1 = np.random.normal(0.,0.5,[num_digits,input_size])
     b1 = np.random.normal(0.,0.5,[num_digits,1])
     
-    #Load the MNIST digit data
+    #Load the MNIST digit data, and provide an example input
     M = load_mnist()
     x = M["train5"][148:149]
     
+    # Calculate the output, L1 is the node values in the first layer
     L1, output = part2_forward(x, W1, b1)
-    #get the index at which the output is the largest
+    
     print x.shape
     print L1
     print output
+    
+    return 0
 
 # Part 3
 # -----------------------------------------------------------------------------
 def part3_backward(y,output,x):
+    """
+    Backward pass of the nn built in part2
+    """
     dy = softmax_backward(y,output)
     dW,db = fully_connected_layer_backward(dy,x)
     print dW.shape
@@ -155,34 +216,39 @@ def part3_backward(y,output,x):
     return dW, db
     
 def part3_grad_check(training_x, training_y, test_x, test_y):
-    
+    """
+    To do: improve this function to perform gradient check and include in the report
+    """
     h = 0.0000001
-    
+    #create weights
     W1 = np.random.normal(0.,0.5,[num_digits,input_size])
     b1 = np.random.normal(0.,0.5,[num_digits,1])
     
+    W1p = W1.copy()
+    b1p = b1.copy()
+    b1p[5] += h
+    
     L1_1, output_1 = part2_forward(training_x, W1, b1)
+    L1_2, output_2 = part2_forward(training_x, W1, b1p)
     
     dW,db = part3_backward(training_y.T,output_1,training_x)
     
     print db[5]
-    
-    b1[5] += h
-    L1_2, output_2 = part2_forward(training_x, W1, b1)
-    
     finite_diff = (NLL(training_y.T, output_2)-NLL(training_y.T, output_1))/h
-    
     print finite_diff
-    
     return 0
 
 def part3():
+    """
+    Perform gradient check
+    """
     #Load the MNIST digit data
     training_x, training_y, test_x, test_y = create_sets()
-      
-    #create weights
+    
     np.random.seed(0)
     part3_grad_check(training_x, training_y, test_x, test_y)
+    
+    return 0
  
 # Part 4
 # -----------------------------------------------------------------------------
@@ -277,7 +343,7 @@ if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
     #part1()
     #part2()
-    #part3()
-    part4()
-    part5()
+    part3()
+    #part4()
+    #part5()
     
