22641e8ebd4b923c7af12eb6e98377672a5cdd06
Yuchen Wu <cheneyuwu@gmail.com>
2018-02-17 21:30:10 -0500

    1

--- CSC411/Project_2/P2_Test/faces.py
+++ CSC411/Project_2/P2_Test/faces.py
@@ -21,6 +21,8 @@ import urllib
 from torch.autograd import Variable
 import torch
 from scipy.io import loadmat
+from torch.utils.data import Dataset, DataLoader,TensorDataset
+import hashlib
 
 # Constants
 # -----------------------------------------------------------------------------
@@ -64,20 +66,21 @@ def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
     else:
         return it.result
 
-"""
-A randomized algorithm that builds training, validation and test set
-Take:
-    act: a list of full name of actors
-    act_num: a dictionary stores available pictures of each actor
-    sets_size_pact: total number of pictures of training+validation+test set for each actor
-    vsize_pact: number of pictures in validation set for each actor
-    tsize_pact: number of pictures in test set for each actor
-Return:
-    training_set: a dictionary stores pictures used in training set
-    validation_set: a dictionary stores pictures used in validation set
-    test_set: a dictionary stores pictures used in test set
-"""
-def build_sets(act,act_num, sets_size_pact,vsize_pact, tsize_pact,seed=0):
+def build_sets(act,act_num, sets_size_pact,vsize_pact, tsize_pact,seed=0, same_num_pact = False):
+
+    """
+    A randomized algorithm that builds training, validation and test set
+    Take:
+        act: a list of full name of actors
+        act_num: a dictionary stores available pictures of each actor
+        sets_size_pact: total number of pictures of training+validation+test set for each actor
+        vsize_pact: number of pictures in validation set for each actor
+        tsize_pact: number of pictures in test set for each actor
+    Return:
+        training_set: a dictionary stores pictures used in training set
+        validation_set: a dictionary stores pictures used in validation set
+        test_set: a dictionary stores pictures used in test set
+    """
     training_set = {}
     validation_set = {}
     test_set = {}
@@ -88,12 +91,13 @@ def build_sets(act,act_num, sets_size_pact,vsize_pact, tsize_pact,seed=0):
         validation_set[a] = []
         test_set[a] = []
         total_num = act_num[a]
-        i = act_num[a]-sets_size_pact
-        while i >0:
-            pop_index = int(round((total_num-1)*np.random.rand()))
-            pop_im = training_set[a].pop(pop_index)
-            i -= 1
-            total_num-=1
+        if same_num_pact:
+            i = act_num[a]-sets_size_pact
+            while i >0:
+                pop_index = int(round((total_num-1)*np.random.rand()))
+                pop_im = training_set[a].pop(pop_index)
+                i -= 1
+                total_num-=1
         i = vsize_pact
         while i >0:
             pop_index = int(round((total_num-1)*np.random.rand()))
@@ -163,98 +167,74 @@ def create_set(classify_act, desired_set):
                                 im = imread("cropped_images/" + image + ".JPEG")
                             except IOError:
                                 im = imread("cropped_images/"+image+".png")
-            im = (im/255.).reshape(1024,1)
+            try:
+                im = (im/255.).reshape(1024,1)
+            except:
+                try:
+                    im = (im[:,:,0]/255.).reshape(1024,1)
+                except:
+                    print ("Cannot reshape image.")
             feature = np.hstack((feature,im))
     return feature.T,label.T
 
-# Part 8_1
-# -----------------------------------------------------------------------------
-def part_1_imdl(source_file_name,folder_name):
+def get_imag_num(source_file_names, foldername):
     
-    try:
-        os.makedirs(folder_name)
-    except OSError:
-        pass
-    
+    image_names = os.listdir('./'+foldername)
+    image_num = {}
     #get the name of actors
-    act = list(set([a.split("\t")[0] for a in open(source_file_name).readlines()]))
-    
-    testfile = urllib.URLopener()            
-    
-    #Note: you need to create the uncropped folder first in order 
-    #for this to work
+    act = []
+    for source_file_name in source_file_names:
+        act+=list(set([a.split("\t")[0] for a in open(source_file_name).readlines()]))
     
     for a in act:
         name = a.split()[1].lower()
-        i = 0
-        for line in open(source_file_name):
-            if a in line.split("\t"):
-                filename = name+str(i)+'.'+line.split()[4].split('.')[-1]
-                #A version without timeout (uncomment in case you need to 
-                #unsupress exceptions, which timeout() does)
-                #testfile.retrieve(line.split()[4], "actors_uncropped/"+filename)
-                #timeout is used to stop downloading images which take too long to download
-                timeout(testfile.retrieve, (line.split()[4], folder_name+filename), {}, 30)
-                if not os.path.isfile(folder_name+filename):
-                    continue
-    
-                #crop and resize the images here
-                try:
-                    im = imread(folder_name+filename)
-                except IOError:
-                    os.remove(folder_name+filename)
-                    print("Cannot read image")
-                    continue
-                
-                crop = line.split("\t")[4].split(",")
-                if im.ndim == 3:
-                    imc = im[int(crop[1]):int(crop[3]),int(crop[0]):int(crop[2]),:]
-                    result = rgb2gray(imresize(imc,(32,32)))
-                else:
-                    imc = im[int(crop[1]):int(crop[3]),int(crop[0]):int(crop[2])]
-                    result = (imresize(imc,(32,32)))/255.
-                #imsave("cropped_images/"+filename, result)
-                plt.imsave("cropped_images/"+filename, result,cmap = cm.gray)
-                
-                print(filename)
+        i = -1
+        for image in image_names:
+            if name in image:
                 i += 1
-def part8_1():
-    try:
-        os.makedirs("cropped_images")
-    except OSError:
-        pass
-    part_1_imdl("facescrub_actors.txt","actors_uncropped/")
-    part_1_imdl("facescrub_actresses.txt","actresses_uncropped/")
+        image_num[name] = i
+    return image_num
 
-# Part 8_2
+# Part 8_1
 # -----------------------------------------------------------------------------
-def part8_2():
-    print ("Part 6d")
+def part8_1():
     try:
         os.makedirs('figures')
     except OSError:
         pass
     
+    full_image_num = get_imag_num(["facescrub_actors.txt","facescrub_actresses.txt"], "cropped_images")
     act =['Lorraine Bracco', 'Peri Gilpin', 'Angie Harmon', 'Alec Baldwin', 'Bill Hader', 'Steve Carell']
-    act_num = {'Lorraine Bracco':119, 'Peri Gilpin':87, 'Angie Harmon':127, 'Alec Baldwin':136, 'Bill Hader':139, 'Steve Carell':133}
-    sets_size_pact = 80
+    
+    act_num = {}
+    
+    for a in act:
+        act_num[a] = full_image_num[a.split()[1].lower()]
+        
+    sets_size_pact = 50
     vsize_pact = 10
-    tsize_pact = 10
+    tsize_pact = 20
     
     training_set,validation_set,test_set = build_sets(act,act_num, sets_size_pact,vsize_pact, tsize_pact)
 
     classify_act = {'Lorraine Bracco':[[1],[0],[0],[0],[0],[0]], 'Peri Gilpin':[[0],[1],[0],[0],[0],[0]], 'Angie Harmon':[[0],[0],[1],[0],[0],[0]], 'Alec Baldwin':[[0],[0],[0],[1],[0],[0]], 'Bill Hader':[[0],[0],[0],[0],[1],[0]], 'Steve Carell':[[0],[0],[0],[0],[0],[1]]}
-    anti_classify_act = {0:'Lorraine Bracco', 1:'Peri Gilpin', 2:'Angie Harmon', 3:'Alec Baldwin', 4:'Bill Hader', 5:'Steve Carell'}
+    #anti_classify_act = {0:'Lorraine Bracco', 1:'Peri Gilpin', 2:'Angie Harmon', 3:'Alec Baldwin', 4:'Bill Hader', 5:'Steve Carell'}
     
-    data,dlabel = create_set(classify_act, training_set)
-    test,tlabel = create_set(classify_act, test_set)
-    validation,vlabel = create_set(classify_act, validation_set)
+    train_x,train_y = create_set(classify_act, training_set)
+    test_x,test_y = create_set(classify_act, test_set)
+    validation_x,validation_y = create_set(classify_act, validation_set)
     
-    return data,dlabel,test,tlabel,validation,vlabel 
+    
+    train_xy = np.hstack((train_x,np.argmax(train_y, 1).reshape((train_x.shape[0],1))))
+    validation_xy = np.hstack((validation_x,np.argmax(validation_y, 1).reshape((validation_x.shape[0],1))))
+    test_xy = np.hstack((test_x,np.argmax(test_y, 1).reshape((test_x.shape[0],1))))
+    
+    
+    return train_xy,validation_xy,test_xy
 
-# Part 8_3
+# Part 8_2
 # -----------------------------------------------------------------------------
-def part8_3(data,dlabel,test,tlabel,validation,vlabel):
+def part8_2_fullbatch(train_xy, validation_xy,test_xy):
     
     dim_x = 32*32
     dim_h = 20
@@ -263,14 +243,16 @@ def part8_3(data,dlabel,test,tlabel,validation,vlabel):
     dtype_float = torch.FloatTensor
     dtype_long = torch.LongTensor 
     
-    x = Variable(torch.from_numpy(data), requires_grad=False).type(dtype_float)
-    y_classes = Variable(torch.from_numpy(np.argmax(dlabel, 1)), requires_grad=False).type(dtype_long)
+    x = Variable(torch.from_numpy(train_xy[:,:-1]), requires_grad=False).type(dtype_float)
+    y_classes = Variable(torch.from_numpy(train_xy[:,-1]), requires_grad=False).type(dtype_long)
     
     model = torch.nn.Sequential(
-            torch.nn.Linear(dim_x, dim_h),
+            torch.nn.Linear(dim_x, dim_out),
             torch.nn.ReLU(),
-            torch.nn.Linear(dim_h, dim_out),
+            torch.nn.Linear(dim_out, dim_out),
+            torch.nn.Softmax()
             )
+    
     loss_fn = torch.nn.CrossEntropyLoss()
     learning_rate = 1e-4
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -282,15 +264,111 @@ def part8_3(data,dlabel,test,tlabel,validation,vlabel):
         loss.backward()    # Compute the gradient
         optimizer.step()   # Use the gradient information to 
         
-    x = Variable(torch.from_numpy(test), requires_grad=False).type(dtype_float)
+    x = Variable(torch.from_numpy(test_xy[:,:-1]), requires_grad=False).type(dtype_float)
     y_pred = model(x).data.numpy()
-    print (np.mean(np.argmax(y_pred, 1) == np.argmax(tlabel, 1)))
-   
+    print (np.mean(np.argmax(y_pred, 1) == test_xy[:,-1]))
+    
+def part8_2(plot = True):
+    
+    train_xy, validation_xy,test_xy =part8_1()
+    
+    dim_x = 32*32
+    dim_h = 20
+    dim_out = 6
+    
+    n_epoch = 4000
+    batch_size = 128
+    
+    dtype_float = torch.FloatTensor
+    dtype_long = torch.LongTensor
+    
+
+    model = torch.nn.Sequential(
+            torch.nn.Linear(dim_x, dim_h),
+            torch.nn.ReLU(),
+            torch.nn.Linear(dim_h, dim_out),
+            torch.nn.Softmax()
+            )
+    """
+    model = torch.nn.Sequential(
+            torch.nn.Linear(dim_x, dim_out),
+            torch.nn.ReLU(),
+            torch.nn.Linear(dim_out, dim_out),
+            torch.nn.Softmax()
+            )
+    """
+    
+    dataloader = DataLoader(train_xy, batch_size=batch_size,shuffle=True)
+    
+    loss_fn = torch.nn.CrossEntropyLoss()
+    learning_rate = 1e-4
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    
+    train_x = Variable(torch.from_numpy(train_xy[:,:-1]), requires_grad=False).type(dtype_float)
+    validation_x = Variable(torch.from_numpy(validation_xy[:,:-1]), requires_grad=False).type(dtype_float)
+    performance_train = []
+    performance_validation = []
+    num_epoch = []
+    
+    for epoch in range(n_epoch):
+        for i, data in enumerate(dataloader):
+            x = Variable(data[:,:-1], requires_grad=False).type(dtype_float)
+            y_classes = Variable(data[:,-1], requires_grad=False).type(dtype_long)
+            
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y_classes)
+        
+            model.zero_grad()  # Zero out the previous gradient computation
+            loss.backward()    # Compute the gradient
+            optimizer.step()   # Use the gradient information to
+        
+        if epoch % 5 ==0:
+            num_epoch.append(epoch)
+            y_pred_train_x = model(train_x).data.numpy()
+            performance_train.append((np.mean(np.argmax(y_pred_train_x, 1) == train_xy[:,-1])))
+            #print (np.mean(np.argmax(y_pred_train_x, 1) == train_xy[:,-1]))
+            y_pred_validation_x = model(validation_x).data.numpy()
+            performance_validation.append((np.mean(np.argmax(y_pred_validation_x, 1) == validation_xy[:,-1])))
+            #print (np.mean(np.argmax(y_pred_validation_x, 1) == validation_xy[:,-1]))
+        
+    if plot==True:
+        plt.plot(num_epoch, performance_train,'-',num_epoch, performance_validation,'-')
+        plt.legend(['training','validation'])
+        plt.xlabel('Number of Epoches')
+        plt.ylabel('Performance')
+        plt.savefig('figures/part8f1.jpg')
+        plt.show()
+        
+        test_x = Variable(torch.from_numpy(test_xy[:,:-1]), requires_grad=False).type(dtype_float)
+        y_pred = model(test_x).data.numpy()
+        print ("Performance on test set is:")
+        print (np.mean(np.argmax(y_pred, 1) == test_xy[:,-1]))
+    
+    return model
+    
+# Part 8_3
+# -----------------------------------------------------------------------------
+    
+def part8_3():
+
+    model = part8_2(False)
+    
+    for i in range(5):
+        print (str(i)+"'s node is:")
+        plt.imshow(model[0].weight.data.numpy()[i, :].reshape((32, 32)), cmap=plt.cm.coolwarm)
+        plt.savefig('figures/part8f'+str(i+2)+'.jpg')
+        plt.show()
+    
+    
+    
+    
 if __name__ == "__main__":
     #Important Note: Please uncomment the following line when using an IDE!
     os.chdir(os.path.dirname(__file__))
-    data,dlabel,test,tlabel,validation,vlabel =part8_2()
-    part8_3(data,dlabel,test,tlabel,validation,vlabel)
+    #train_xy,validation_xy,test_xy = part8_1()
+    #part8_2()
+    part8_3()
+
 
 # Test Code
 # -----------------------------------------------------------------------------
---
+++ CSC411/Project_2/P2_Test/images.py
@@ -0,0 +1,212 @@
+from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import time
+from copy import deepcopy
+from scipy.misc import imread
+from scipy.misc import imresize
+from scipy.misc import imsave
+import matplotlib.image as mpimg
+import os
+from scipy.ndimage import filters
+import urllib
+from torch.autograd import Variable
+import torch
+from scipy.io import loadmat
+from torch.utils.data import Dataset, DataLoader,TensorDataset
+import hashlib
+
+# Constants
+# -----------------------------------------------------------------------------
+
+
+# Functions used in multiple parts
+# -----------------------------------------------------------------------------
+def rgb2gray(rgb):
+    '''Return the grayscale version of the RGB image rgb as a 2D numpy array
+    whose range is 0..1
+    Arguments:
+    rgb -- an RGB image, represented as a numpy array of size n x m x 3. The
+    range of the values is 0..255
+    '''
+    
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray/255.
+
+def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
+    '''From:
+    http://code.activestate.com/recipes/473878-timeout-function-using-threading/'''
+    import threading
+    class InterruptableThread(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+            
+            self.result = None
+
+        def run(self):
+            try:
+                self.result = func(*args, **kwargs)
+            except:
+                self.result = default
+
+    it = InterruptableThread()
+    it.start()
+    it.join(timeout_duration)
+    if it.isAlive():
+        return False
+    else:
+        return it.result
+
+def sha256_check(filename, block_size = 65536):
+    """
+    sha256 check sum
+    """
+    sha256 = hashlib.sha256()
+    with open(filename, 'rb') as f:
+        for block in iter(lambda: f.read(block_size), b''):
+            sha256.update(block)
+    return sha256.hexdigest()
+
+
+# Part 8_1
+# -----------------------------------------------------------------------------
+def part_1_imdl(source_file_name,folder_name,folder_name2):
+    
+    try:
+        os.makedirs(folder_name)
+    except OSError:
+        pass
+    
+    #get the name of actors
+    act = list(set([a.split("\t")[0] for a in open(source_file_name).readlines()]))
+    
+    testfile = urllib.URLopener()            
+    
+    #Note: you need to create the uncropped folder first in order 
+    #for this to work
+    
+    for a in act:
+        name = a.split()[1].lower()
+        i = 0
+        for line in open(source_file_name):
+            if a in line.split("\t"):
+                filename = name+str(i)+'.'+line.split()[4].split('.')[-1]
+                #A version without timeout (uncomment in case you need to 
+                #unsupress exceptions, which timeout() does)
+                #testfile.retrieve(line.split()[4], "actors_uncropped/"+filename)
+                #timeout is used to stop downloading images which take too long to download
+                timeout(testfile.retrieve, (line.split()[4], folder_name+filename), {}, 30)
+                if not os.path.isfile(folder_name+filename):
+                    continue
+                
+                #sha256 check
+                check = sha256_check(folder_name+filename)
+                if not check == line.split()[6]:
+                    print "SHA256 does not match! Delete file."
+                    time.sleep(5)
+                    try:
+                        os.remove(folder_name+filename)
+                    except:
+                        print "Can not remove file!"
+                    continue
+    
+                #crop and resize the images here
+                try:
+                    im = imread(folder_name+filename)
+                except IOError:
+                    print"Cannot read image"
+                    time.sleep(5)
+                    try:
+                        os.remove(folder_name+filename)
+                    except:
+                        print "Can not remove file!"                 
+                    continue
+                
+                crop = line.split("\t")[4].split(",")
+                if im.ndim == 3:
+                    imc = im[int(crop[1]):int(crop[3]),int(crop[0]):int(crop[2]),:]
+                    result = rgb2gray(imresize(imc,(32,32)))
+                else:
+                    imc = im[int(crop[1]):int(crop[3]),int(crop[0]):int(crop[2])]
+                    result = (imresize(imc,(32,32)))/255.
+                #imsave("cropped_images/"+filename, result)
+                plt.imsave(folder_name2+filename, result,cmap = cm.gray)
+                
+                print(filename)
+                i += 1
+                
+def get_imag_num(source_file_names, foldername):
+    
+    image_names = os.listdir('./'+foldername)
+    image_num = {}
+    #get the name of actors
+    act = []
+    for source_file_name in source_file_names:
+        act+=list(set([a.split("\t")[0] for a in open(source_file_name).readlines()]))
+    
+    for a in act:
+        name = a.split()[1].lower()
+        i = -1
+        for image in image_names:
+            if name in image:
+                i += 1
+        image_num[name] = i
+    return image_num
+        
+def delete_imag(folder_name, image_num, delete_images):
+    num = image_num.copy()
+    
+    for delete_image in delete_images:
+        time.sleep(5)
+        try:
+            os.remove(folder_name+delete_image)
+        except:
+            print "Can not remove file!"
+            break
+        for name in num.keys():
+            if name in delete_image:
+                try:
+                    os.rename(folder_name+name+str(num[name])+".jpg",folder_name+delete_image.split(".")[0]+".jpg")
+                except:
+                    try:
+                        os.rename(folder_name+name+str(num[name])+".JPG",folder_name+delete_image.split(".")[0]+".JPG")
+                    except:
+                        try:
+                            os.rename(folder_name+name+str(num[name])+".jpeg",folder_name+delete_image.split(".")[0]+".jpeg")
+                        except:
+                            try:
+                                os.rename(folder_name+name+str(num[name])+".JPEG",folder_name+delete_image.split(".")[0]+".JPEG")
+                            except:
+                                try:
+                                    os.rename(folder_name+name+str(num[name])+".png",folder_name+delete_image.split(".")[0]+".png")
+                                except:
+                                    try:
+                                        os.rename(folder_name+name+str(num[name])+".PNG",folder_name+delete_image.split(".")[0]+".PNG")
+                                    except:  
+                                        print "Can not rename file!"
+                                        break
+                num[name] -= 1
+
+def part8_get_faces():
+    try:
+        os.makedirs("cropped_images")
+    except OSError:
+        pass
+    
+    part_1_imdl("facescrub_actors.txt","actors_uncropped/","cropped_images/")
+    part_1_imdl("facescrub_actresses.txt","actresses_uncropped/","cropped_images/")
+    
+    image_num = get_imag_num(["facescrub_actors.txt","facescrub_actresses.txt"], "cropped_images")
+    
+    #this is hard code part, do not run it twice!
+    delete_imag("cropped_images/", image_num, ["chenoweth71.jpg","baldwin91.jpg","drescher69.jpg","drescher81.jpg","drescher31.jpg"])
+    
+    
+    
+if __name__ == "__main__":
+    #Important Note: Please uncomment the following line when using an IDE!
+    os.chdir(os.path.dirname(__file__))
+    part8_get_faces()
