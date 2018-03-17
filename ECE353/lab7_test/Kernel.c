ba76a337c06a0e9acb6a7d71463cf3d7b54f401b
Yuchen Wu <cheneyuwu@gmail.com>
2018-03-17 02:22:41 -0400

    1

--- ECE353/lab7_test/Kernel.c
+++ ECE353/lab7_test/Kernel.c
@@ -1029,11 +1029,13 @@ code Kernel
         --
         -- This method is passed a ptr to a Process;  It moves it
         -- to the FREE list.
-				processManagerLock.Lock()
-				(*p).status = FREE
-				freeList.AddToEnd(p)
-				aProcessBecameFree.Signal(&processManagerLock)
-				processManagerLock.Unlock()
+        --
+          -- NOT IMPLEMENTED
+          processManagerLock.Lock()
+          (*p).status = FREE
+          freeList.AddToEnd(p)
+          aProcessBecameFree.Signal(&processManagerLock)
+          processManagerLock.Unlock()
         endMethod
 
     endBehavior
